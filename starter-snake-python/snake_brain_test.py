f4bb978fb292a799884c5045a0aacf8f3b8438a7
Adam Loving <github@adamloving.com>
2021-05-27 09:09:38 -0700

    add fixture to test

--- starter-snake-python/snake_brain_test.py
+++ starter-snake-python/snake_brain_test.py
@@ -1,10 +1,18 @@
 import unittest
+import json
 
 from snake_brain import SnakeBrain
 
 class TestMoving(unittest.TestCase):
 
+    def setUp(self):
+        with open('fixtures/example_turn.json') as f:
+            self.data = json.load(f)
+
     def test_move(self):
         snake = SnakeBrain()
-        self.assertEqual(snake.get_move({}), "left")
+        self.assertIn(snake.get_move(self.data),
+            ["left", "right", "up", "down"])
 
+if __name__ == '__main__':
+    unittest.main()
