871aaf50f0b8c6eff03294654098e10a3a7e1744
Adam Loving <github@adamloving.com>
2021-07-17 12:00:19 -0700

    checkpoint

---
+++ starter-snake-python/maximax_test.py
@@ -0,0 +1,25 @@
+import unittest
+import json
+
+from board import Board
+from maximax import maximax
+
+# to run in console me use: python snake_brain_test.py
+class MaximaxTest(unittest.TestCase):
+
+    def setUp(self):
+        with open('fixtures/three_snakes.json') as f:
+            self.data = json.load(f)
+
+    def xtest_maximax_depth0(self):
+        b = Board(self.data["board"], self.data["you"]["id"])
+        b.print()
+        print(maximax(b, 0))
+
+    def test_maximax_depth(self):
+        b = Board(self.data["board"], self.data["you"]["id"])
+        b.print()
+        print(maximax(b, 5))
+
+if __name__ == '__main__':
+    unittest.main()
